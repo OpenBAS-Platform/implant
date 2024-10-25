6e372c3e0595d31d315f4a05cdfd264abf92a999
Damien Goujard <damien.goujard@filigran.io>
2024-10-25 17:17:48 +0200

    [implant] command_execution with all executors (must refacto)

--- src/process/command_exec.rs
+++ src/process/command_exec.rs
@@ -14,55 +14,79 @@ pub struct ExecutionResult {
     pub exit_code: i32,
 }
 
-pub fn invoke_command(command: &str, executor: &str) -> std::io::Result<Output> {
+pub fn invoke_command(command: &str, executor: &str, args: &[&str]) -> std::io::Result<Output> {
     Command::new(executor)
-        .arg("-c")
-        .arg(&command)
+        .args(args)
+        .arg(command)
         .stderr(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()?
         .wait_with_output()
 }
 
+// TODO refacto all on monday, just save for the weekend
 #[cfg(target_os = "windows")]
 pub fn command_execution(command: &str, executor: &str, pre_check: bool) -> Result<ExecutionResult, Error> {
     let invoke_output;
-    // TODO encode/decode base64 before ? + erreurs dans l'encart en bleu (test) ?
     if executor == "cmd" {
-        invoke_output = Command::new("cmd.exe")
-            .args(&[
-                "/d",
-                "/c",
-                &command
-            ])
-            .stderr(Stdio::piped())
+        // To manage multi lines (we need more than just base64 like the other executor)
+        // TODO warning \n can be found in windows path !?
+        let new_command = command.replace("\n", " & ");
+        let invoke_expression = format!("([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(new_command));
+        let echo_cmd = Command::new("powershell.exe")
+            .arg(&invoke_expression)
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        invoke_output = Command::new("cmd")
+            .stdin(Stdio::from(echo_cmd.stdout.unwrap()))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()?
             .wait_with_output();
     } else if executor == "bash" {
-        invoke_output = invoke_command(command, executor);
-    } else if executor == "sh" {
-        invoke_output = invoke_command(command, executor);
-    } else {
-        let invoke_expression = format!("Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(command));
-        invoke_output = Command::new("cmd.exe")
-            .args(&[
-                "/d",
-                "/c",
-                "powershell.exe",
-                "-ExecutionPolicy",
-                "Bypass",
-                "-WindowStyle",
-                "Hidden",
-                "-NonInteractive",
-                "-NoProfile",
-                "-Command",
-                &invoke_expression,
-            ])
-            .stderr(Stdio::piped())
+        let echo_cmd = Command::new("bash")
+            .arg("-c")
+            .arg("echo ".to_owned() + &BASE64_STANDARD.encode(command) + " | base64 --d")
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        invoke_output = Command::new("bash")
+            .stdin(Stdio::from(echo_cmd.stdout.unwrap()))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()?
             .wait_with_output();
+    } else if executor == "sh" {
+        let echo_cmd = Command::new("sh")
+            .arg("-c")
+            .arg("echo ".to_owned() + &BASE64_STANDARD.encode(command) + " | base64 --d")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        invoke_output = Command::new("sh")
+            .stdin(Stdio::from(echo_cmd.stdout.unwrap()))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?
+            .wait_with_output();
+    } else {
+        // For powershell complex command, we need to encode in base64 to manage escape caracters
+        let invoke_expression = format!("Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(command));
+        invoke_output = invoke_command(&invoke_expression, "cmd.exe", &[
+            "/d",
+            "/c",
+            "powershell.exe",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-WindowStyle",
+            "Hidden",
+            "-NonInteractive",
+            "-NoProfile",
+            "-Command"]);
     }
     let invoke_result = invoke_output.unwrap().clone();
     // 0 success | other = maybe prevented
@@ -93,11 +117,31 @@ pub fn command_execution(command: &str, executor: &str, pre_check: bool) -> Resu
 pub fn command_execution(command: &str, executor: &str, pre_check: bool) -> Result<ExecutionResult, Error> {
     let invoke_output;
     if executor == "bash" {
-        invoke_output = invoke_command(command, executor);
+        // For shell complex command, we need to encode in base64 to manage escape caracters
+        let echo_child = Command::new("echo")
+            .arg(BASE64_STANDARD.encode(command))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let base64_child = Command::new("base64")
+            .arg("-d")
+            .stdin(Stdio::from(echo_child.stdout.unwrap()))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap();
+        invoke_output = Command::new("bash")
+            .stdin(Stdio::from(base64_child.stdout.unwrap()))
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()?
+            .wait_with_output();
     } else if executor == "psh" {
         let invoke_expression = format!("Invoke-Expression ([System.Text.Encoding]::UTF8.GetString([convert]::FromBase64String(\"{}\")))", BASE64_STANDARD.encode(command));
-        invoke_output = invoke_command(invoke_expression, "powershell");
+        invoke_output = invoke_command(&invoke_expression, "powershell", &["-c"]);
     } else {
+        // For shell complex command, we need to encode in base64 to manage escape caracters
         let echo_child = Command::new("echo")
             .arg(BASE64_STANDARD.encode(command))
             .stdout(Stdio::piped())
