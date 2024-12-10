328d1751ddbe5b922c6a488d24bed839a914705e
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2024-12-10 09:05:53 +0100

    removed executor mapping

--- src/process/exec_utils.rs
+++ src/process/exec_utils.rs
@@ -4,13 +4,6 @@ use std::process::Command;
 
 
 pub fn is_executor_present(executor: &str) -> bool {
-
-    // add a mapping to get the executor name, if not in the map we use the parameter
-    let alias_map: HashMap<&str, &str> = HashMap::from([
-        ("psh", "powershell"),
-    ]);
-    let actual_executor = alias_map.get(executor).unwrap_or(&executor);
-
     Command::new(actual_executor)
         .spawn()
         .map(|mut child| child.kill().is_ok()) // Kill immediately to avoid hanging.
