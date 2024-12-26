4fd08c79aa80b1714842885025fbcedbb02ae7b8
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2024-12-26 09:48:01 +0100

    removed deprecated method tls_config_with_provider

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 use std::time::Duration;
+use rustls::{ClientConfig, RootCertStore};
 use ureq::{Agent, Request};
 
 pub mod manage_inject;
@@ -21,12 +22,14 @@ impl Client {
             .timeout(Duration::from_secs(5))
             .user_agent(format!("openbas-implant/{}", VERSION).as_str())
             .try_proxy_from_env(with_proxy);
+
         if unsecured_certificate {
-            let arc_crypto_provider = Arc::new(rustls::crypto::ring::default_provider());
-            let config = rustls_platform_verifier::tls_config_with_provider(arc_crypto_provider)
-                .expect("Failed to create TLS config with crypto provider");
+            let config = ClientConfig::builder()
+                .with_root_certificates(RootCertStore::empty())
+                .with_no_client_auth();
             http_client = http_client.tls_config(Arc::new(config));
         }
+
         // Remove trailing slash
         let mut url = server_url;
         if url.ends_with('/') {
