035a23f441a15c6463088069b94c1e4f881143de
Hedi Tarchouni <hedi.tarchouni@filigran.io>
2025-05-26 16:53:47 +0200

    added a condition to only compile raw_arg on windows

--- src/process/command_exec.rs
+++ src/process/command_exec.rs
@@ -28,12 +28,14 @@ pub fn invoke_command(
     args: &[&str],
 ) -> std::io::Result<Output> {
 
-let result = match executor {
+  let mut command = Command::new(executor);
+
+  let result = match executor {
     // For CMD we use "raw_args" to fix issue #3161;
     #[cfg(windows)]
-    "cmd" => Command::new(executor).args(args).raw_arg(cmd_expression),
+    "cmd" => command.args(args).raw_arg(cmd_expression),
     // for other executors, we still use "args" as they are working properly.
-    _  => Command::new(executor).args(args).arg(cmd_expression),
+    _  => command.args(args).arg(cmd_expression),
   }
   .stdout(Stdio::piped())
   .output();
