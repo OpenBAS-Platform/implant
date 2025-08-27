70fc8aea181b8a72fa825e9ac79fc8fba2fb58fe
Eva Ezzeribi <eva.ezzeribi@filigran.io>
2025-08-27 16:00:37 +0200

    [backend] Fix file name with special char; add decode filename after import;

--- src/process/exec_utils.rs
+++ src/process/exec_utils.rs
@@ -1,3 +1,5 @@
+use crate::common::error_model::Error;
+use percent_encoding::percent_decode_str;
 use std::process::Command;
 
 pub fn is_executor_present(executor: &str) -> bool {
@@ -5,4 +7,11 @@ pub fn is_executor_present(executor: &str) -> bool {
         .spawn()
         .map(|mut child| child.kill().is_ok())
         .unwrap_or(false)
+}
+
+pub fn decode_filename(name: &str) -> Result<String, Error> {
+    percent_decode_str(name)
+        .decode_utf8()
+        .map(|cow| cow.into_owned())
+        .map_err(|err| Error::Internal(format!("Invalid filename: {}", err)))
 }
